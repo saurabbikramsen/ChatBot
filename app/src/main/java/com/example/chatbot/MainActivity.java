668360ecc1463c7c4cf821047f2ad666cfd2f15a
saurabbikramsen <113790283+saurabbikramsen@users.noreply.github.com>
2023-03-03 12:52:36 +0545

    voice on/off feature added, app icon added

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.chatbot;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -27,9 +33,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Objects;
-
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -40,16 +46,21 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnUtteranceCompletedListener {
 
     private RecyclerView chatsRV;
     private EditText userMsgEdit;
     private FloatingActionButton sendMsgFAB;
     private ChatRVAdapter chatRVAdapter;
-    private String secret_key = "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";
+    private String secret_key = "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";
     OkHttpClient client = new OkHttpClient();
     int REQUEST_CODE_SPEECH_INPUT = 22222;
     TextToSpeech t1;
+
+    boolean autoSpeck = true;
+    MediaPlayer mp;
+
 
 
     public static final MediaType JSON
@@ -60,14 +71,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+       actionBar.setHomeAsUpIndicator(R.drawable.bot45real);// set drawable icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        actionBar.setIcon(R.drawable.bot);
+        actionBar.setTitle("VoiceMate");
+
+
+        mp = MediaPlayer.create(this, R.raw.google);
+
         t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status != TextToSpeech.ERROR) {
                     t1.setLanguage(Locale.UK);
+
+                }
+                if(status == TextToSpeech.SUCCESS) {
+                    t1.setOnUtteranceCompletedListener(MainActivity.this);
                 }
             }
+
+
         });
+
 
 
         chatsRV = findViewById(R.id.idRVChats);
@@ -76,22 +105,8 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.txt_mic).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent
-                        = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
-                        Locale.getDefault());
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+           openGoogleAssist();
 
-                try {
-                    startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
-                } catch (Exception e) {
-                    Toast
-                            .makeText(MainActivity.this, " " + e.getMessage(),
-                                    Toast.LENGTH_SHORT)
-                            .show();
-                }
             }
 
         });
@@ -116,7 +131,83 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private void  openGoogleAssist(){
+        Intent intent
+                = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+        mp.start();
 
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        } catch (Exception e) {
+            Toast
+                    .makeText(MainActivity.this, " " + e.getMessage(),
+                            Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuitems, menu);
+        MenuItem item = (MenuItem) menu.findItem(R.id.show_secure);
+        item.setActionView(R.layout.menu_entity);
+
+        Switch switchAB = item
+                .getActionView().findViewById(R.id.switchAB);
+        ImageView speak = item
+                .getActionView().findViewById(R.id.speaker);
+        switchAB.setChecked(true);
+
+
+        switchAB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                if (isChecked) {
+                    switchAB.setChecked(true);
+                    autoSpeck = true;
+                } else {
+                    switchAB.setChecked(false);
+                    if(t1.isSpeaking()){
+                        t1.stop();
+                    }
+                    autoSpeck = false;
+                }
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+//        if(item.getItemId()==R.id.btn_switch){
+//            if(btnSwitch.isChecked()){
+//                btnSwitch.setChecked(false);
+//                autoSpeck=false;
+//            }else{
+//                btnSwitch.setChecked(true);
+//                autoSpeck=true;
+//            }
+
+//        }
+        return super.onOptionsItemSelected(item);
+    }
+    //    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+//        inflater.inflate(R.menu.menuitems, menu);
+//        MenuItem menuItem = menu.findItem(R.id.btn_switch);
+//        SwitchCompat mySwitch = (SwitchCompat) menuItem.getActionView();
+//
+//        mySwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//
+//        });
+//
+//    }
 
 
     @Override
@@ -127,8 +218,16 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK && data != null) {
                 ArrayList<String> result = data.getStringArrayListExtra(
                         RecognizerIntent.EXTRA_RESULTS);
-                userMsgEdit.setText(
-                        Objects.requireNonNull(result).get(0));
+                if (autoSpeck) {
+
+//                    chatList.add(new ChatModel(Objects.requireNonNull(result).get(0), ChatModel.user_key));
+//                    chatRVAdapter.notifyDataSetChanged();
+                    networkCall(Objects.requireNonNull(result).get(0));
+
+                } else {
+                    userMsgEdit.setText(
+                            Objects.requireNonNull(result).get(0));
+                }
             }
         }
     }
@@ -137,6 +236,8 @@ public class MainActivity extends AppCompatActivity {
         try {
 
             chatList.add(new ChatModel(user_message, ChatModel.user_key));
+            chatsRV.scrollToPosition(chatList.size());
+
             chatRVAdapter.notifyDataSetChanged();
 
             //okhttp
@@ -167,8 +268,13 @@ public class MainActivity extends AppCompatActivity {
                             jsonObject = new JSONObject(response.body().string());
                             JSONArray jsonArray = jsonObject.getJSONArray("choices");
                             String result = jsonArray.getJSONObject(0).getString("text");
-                                t1.speak(result, TextToSpeech.QUEUE_FLUSH, null);
+                            if (autoSpeck) {
+                                HashMap<String, String> myHashAlarm = new HashMap<String, String>();
+//                                myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_STREAM, String.valueOf(AudioManager.STREAM_ALARM));
+                                myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "SOME MESSAGE");
+                                t1.speak(result, TextToSpeech.QUEUE_FLUSH, myHashAlarm);
 
+                            }
                             addResponse(result.trim());
                         } catch (JSONException e) {
                             Log.d("message", "onResponse: " + e.getMessage());
@@ -188,7 +294,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             });
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
     }
@@ -204,9 +310,30 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 chatList.add(new ChatModel(message, source));
                 chatRVAdapter.notifyDataSetChanged();
+                chatsRV.scrollToPosition(chatList.size());
+
 //                msg_rv.smoothScrollToPosition(messageAdapter.getItemCount());
             }
         });
+    }
+
+    @Override
+    public void onUtteranceCompleted(String utteranceId) {
+        Log.d("completed", "onUtteranceCompleted: ");
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                if(!t1.isSpeaking() && autoSpeck){
+
+                    openGoogleAssist();
+
+                }
+            }
+        }, 2000);
+
+
     }
 
 }
